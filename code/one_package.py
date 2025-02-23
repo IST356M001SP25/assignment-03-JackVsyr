02c63b437dd71385b6da05e33706bd986e7ab6e0
JackVsyr <jgveneru@syr.edu>
2025-02-22 19:34:21 -0500

    modified:   code/one_package.py

--- code/one_package.py
+++ code/one_package.py
@@ -6,3 +6,27 @@ Calculate the total package size and display that.
 
 see one_package.png for a screenshot
 '''
+import streamlit as st
+import packaging
+
+def main():
+    st.title("Data Stuff")
+    
+    file = st.text_input("Enter data:")
+    
+    if file: 
+        
+        st.write(file)
+        x = packaging.parse_packaging(file)
+        p = packaging.calc_total_units(x)
+        p2 = packaging.get_unit(x) 
+        
+        st.write(x)
+        for i in x:
+            name = list(i.keys())[0]
+            size = list(i.values())[0]
+            st.info(f"{name} ➡️ {size}")
+        st.success(f"Total 📦 Size: {p} {p2}")
+
+if __name__ == "__main__":
+    main()
